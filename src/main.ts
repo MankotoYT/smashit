c5c5fe5f11dacc0a542f1ca26d6c9d8f8ac3b391
Leonard Moreno <leonardmoreno02@gamil.com>
2025-04-20 16:38:16 -0600

    stage commit push

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+// CAPLOCK (BLOCL MAYUS) is ESC
+// in NORMAL -> i -> INSERT
+// in INSERT -> ESC -> NORMAL
 
+// NORMAL -> k -> up   (w)
+// NORMAL -> h -> left (a)
+// NORMAL -> j -> down (s)
+// NORMAL -> l -> right(d)
 createApp(App).mount('#app')
-
 import './learning-01'
 import './learning-02'
 import './learning-03'
